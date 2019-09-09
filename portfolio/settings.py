3d0bcc3823e2ff9f39242253e78f6438f0f830b7
avalos010 <luizavalos40@gmail.com>
2019-09-09 18:00:19 -0500

    added bootstrap and jobs are showing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+BLOG_ROOT = os.path.join(BASE_DIR, 'blog')
+BLOG_URL = '/blog/'
