c17f7d1d21dcbf695cf4531def91f1e903ae46ad
avalos010 <luizavalos40@gmail.com>
2019-09-10 16:40:27 -0500

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'durango01',
+        'PASSWORD': 'password123',
         'HOST': 'localhost',
         'PORT': '5432'
     }
